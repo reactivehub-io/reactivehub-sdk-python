f903f7f9cb027c14b0788d65b58aadf1907311a3
Luiz Gustavo Caciatori <luizgustavo.caciatori@gmail.com>
2019-01-20 13:24:44 -0200

    Adjust in package

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 class Config:
   def __init__(self, team_name, client_key, client_secret):
@@ -14,16 +15,6 @@ def publish_event(config, event_name, payload):
             "client_key": config.client_key,
             "client_secret": config.client_secret}
 
-  url = 'https://{}.reactiveuh.io/event/{}`'.format(config.team_name, event_name)
-  print(url)
+  url = 'https://{}.reactivehub.io/event/{}'.format(config.team_name, event_name)
 
-  requests.post(url, data=payload, headers=headers)
-
-
-config = build_client('reactivehub', 'eaab5f13-13b7-4a91-b7c9-d4dbc9a7c276', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lc3BhY2UiOiJyZWFjdGl2ZWh1YiIsImlkIjoiYmMyMWNkMTAtNzYxNi0xMWU4LThhOGMtZDliZjk2YzhiOGEyIiwia2V5IjoiZWFhYjVmMTMtMTNiNy00YTkxLWI3YzktZDRkYmM5YTdjMjc2IiwiaWF0IjoxNTI5NjcwMDY1fQ.Q-JUfXFcVMLnW8h-L6UqYtjOogp-jLeziuEjtjFuYEA')
-publish_event(config, 'new-user', {
-	"email": "luiz@reactivehub.io",
-	"name": "Luiz",
-	"description": "Teste python",
-	"namespace": "reactivehub"
-})
+  return requests.post(url, data=json.dumps(payload), headers=headers)
