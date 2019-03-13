d7c0089f29e30a0be2c035c39005c1d63d238d79
Natan Streppel <streppels@gmail.com>
2019-03-12 23:32:11 -0300

    renaming client name

--- reactivehub.py
+++ reactivehub.py
@@ -1,10 +1,10 @@
 import requests
 import json
 
-class Config:
+class Client:
   def __init__(self, team_name, client_key, client_secret):
     if (team_name == None or client_key == None or client_secret == None):
-      raise ValueError('Empty values are not allowed. Please instantiate a Config class with your team name, client key and client secret')
+      raise ValueError('Empty values are not allowed. Please instantiate a Client class with your team name, client key and client secret')
     self.team_name = team_name
     self.client_key = client_key
     self.client_secret = client_secret
@@ -15,6 +15,3 @@ class Config:
               "client_secret": self.client_secret}
     url = 'https://{team_name}.reactivehub.io/event/{event_name}'.format(team_name=self.team_name, event_name=event_name)
     return requests.post(url, data=json.dumps(payload), headers=headers)
-
-def build_client(team_name, client_key, client_secret):
-  return Config(team_name, client_key, client_secret)
