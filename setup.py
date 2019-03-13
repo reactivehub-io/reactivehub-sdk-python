de31147d55aa80eb7362811f0d49d3bc25e891e7
Natan Streppel <streppels@gmail.com>
2019-03-12 22:29:58 -0300

    changes to setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-import setuptools
+from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
   name='reactivehub-sdk-python',
   version='0.1',
   scripts=[''],
   author='reactivehub.io',
   author_email='luiz.gustavo@reactivehub.io',
   description='reactivehub.io Python SDK',
-  long_description='The reactivehub.io Python SDK implements the methods to publish in Event api'
+  long_description='The Reactivehub Python SDK implements the methods needed to publish events'
   url='https://github.com/reactivehub-io/reactivehub-sdk-python',
   packages=setuptools.find_packages(),
   classifiers=[
