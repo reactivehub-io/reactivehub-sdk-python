181a4d2cb90cb74dd212528c16279d0bf04f2a8e
Natan Streppel <streppels@gmail.com>
2019-03-12 23:09:05 -0300

    adding publish_event to self config class

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ import json
 
 class Config:
   def __init__(self, team_name, client_key, client_secret):
+    if (team_name == None or client_key == None or client_secret == None):
+      raise ValueError('Empty values are not allowed. Please instantiate a Config class with your team name, client key and client secret')
     self.team_name = team_name
     self.client_key = client_key
     self.client_secret = client_secret
 
+  def publish_event(self, event_name, payload):
+    headers = {"Content-type": "application/json",
+              "client_key": self.client_key,
+              "client_secret": self.client_secret}
+    url = 'https://{team_name}.reactivehub.io/event/{event_name}'.format(team_name=self.team_name, event_name=event_name)
+    return requests.post(url, data=json.dumps(payload), headers=headers)
+
 def build_client(team_name, client_key, client_secret):
   return Config(team_name, client_key, client_secret)
-
-def publish_event(config, event_name, payload):
-  headers = {"Content-type": "application/json",
-            "client_key": config.client_key,
-            "client_secret": config.client_secret}
-
-  url = 'https://{}.reactivehub.io/event/{}'.format(config.team_name, event_name)
-
-  return requests.post(url, data=json.dumps(payload), headers=headers)
--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-from setuptools import setup
+from setuptools import setup, setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
   name='reactivehub-sdk-python',
-  version='0.1',
+  version='0.2',
   scripts=[''],
   author='reactivehub.io',
   author_email='luiz.gustavo@reactivehub.io',
   description='reactivehub.io Python SDK',
-  long_description='The Reactivehub Python SDK implements the methods needed to publish events'
+  long_description='The Reactivehub Python SDK implements the methods needed to publish in the Events API.',
   url='https://github.com/reactivehub-io/reactivehub-sdk-python',
   packages=setuptools.find_packages(),
   classifiers=[
