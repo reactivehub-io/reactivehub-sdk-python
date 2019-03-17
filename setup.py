b719dbe309b5c6e14ef692b7ecab1faa9f8dab4b
Luiz Gustavo Caciatori <luizgustavo.caciatori@gmail.com>
2019-03-17 17:31:03 -0300

    Fix license's name

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ setuptools.setup(
   author='reactivehub.io',
   author_email='luiz.gustavo@reactivehub.io',
   description='reactivehub.io Python SDK',
-  long_description='The reactivehub.io Python SDK implements the methods to publish in Event api'
+  long_description='The reactivehub.io Python SDK implements the methods to publish in Event api',
   url='https://github.com/reactivehub-io/reactivehub-sdk-python',
   packages=setuptools.find_packages(),
   classifiers=[
     "Programming Language :: Python :: 3",
-    "License :: Apache 2.0",
+    "License :: OSI Approved :: Apache Software License",
     "Operating System :: OS Independent",
   ],
 )
